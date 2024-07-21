7c34eabc0d00c9032b8c3804287a8818ba06625b
alex_rc6 <sasha.rc6@gmail.com>
2024-07-21 14:26:29 +0500

    Add Home Work to Seminar 2 CSharp

---
+++ Task_2_HW_2DimArray/Program.cs
@@ -0,0 +1,74 @@
+﻿// Дан двумерный массив. Отсортировать данные по возрастанию
+namespace Task_2_HW_2DimArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] a = { { 7, 3, 2 }, { 4, 9, 6 }, { 1, 8, 5 } };
+
+
+            int[] Convert2DimTo1DimArray(int[,] arr)
+            {
+                int arrayX = arr.GetLength(0);
+                int arrayY = arr.GetLength(1);
+
+                var ArrOneDimmensional = new int[arrayX * arrayY];
+
+                for (int i = 0; i < arrayX; i++)
+                {
+                    for (int j = 0; j < arrayY; j++)
+                    {
+                        ArrOneDimmensional[arrayY * i + j] = a[i, j];
+                    }
+
+                }
+                return ArrOneDimmensional;
+            }
+
+            int[,] Convert1DimTo2DimArray(int[] oneDimArr, int rowLength)
+            {
+                int colLenght = oneDimArr.Length / rowLength;
+                var twoDimArr = new int[colLenght, rowLength];
+
+                for (int i = 0; i < colLenght; i++)
+                {
+                    for (int j = 0; j < rowLength; j++)
+                    {
+                        twoDimArr[i, j] = oneDimArr[rowLength * i + j];
+                    }
+                }
+                return twoDimArr;
+            }
+
+            void PrintArray(int[,] arr)
+            {
+                for (int i = 0; i < arr.GetLength(0); i++)
+                {
+                    for (int j = 0; j < arr.GetLength(1); j++)
+                    {
+                        Console.Write(arr[i, j]);
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+
+            PrintArray(a);
+            Console.WriteLine();
+
+            int[] oneDimArr = Convert2DimTo1DimArray(a);
+            Array.Sort(oneDimArr); 
+
+            PrintArray(Convert1DimTo2DimArray(oneDimArr, a.GetLength(1)));
+
+
+
+
+
+
+
+
+        }
+    }
+}
